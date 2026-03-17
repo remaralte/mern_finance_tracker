3cd2b226ba6244caf10186e791cedc627c6ace08
remaralte <rema.ralte154@gmail.com>
2026-03-17 15:50:39 +0530

    clerk set up and auth page

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import {ClerkProvider} from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY){
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

f4701ad21244bf46d3b000e8731a1686f8554deb
remaralte <rema.ralte154@gmail.com>
2026-05-27 21:49:18 +0530

    backend foundation: connected to mongoDB

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from "express";
+import type { Express } from "express"; //this line is added instead of "express, {Express} from" in first line
+import mongoose from "mongoose";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());        //apply middleware into endpoints => allows json into bodies of each request
+
+//setup mongoDB connection
+const mongoURI = process.env.MONGO_URI as string;
+
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log("CONNECTED TO MONGODB!"))
+    .catch((err) => console.error("Failed to connect to MongoDB:", err));
+
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}`);
+});
